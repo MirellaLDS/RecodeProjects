f17558973f84bc6d2699ef5a166c189f5ff548ec
Mirella <mirella.lds@gmail.com>
2021-07-20 20:47:07 -0300

    count and toast

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.helloandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToastMessage(View view) {
+        Toast.makeText(this, "Hello Toast", Toast.LENGTH_LONG).show();
+    }
+
+    public void goAcount(View view) {
+        contador += 1;
+
+        TextView campoDeTexto = findViewById(R.id.tvContador);
+        campoDeTexto.setText(Integer.toString(contador));
     }
 }
