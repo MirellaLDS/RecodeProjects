2e0fc482c52a260898c1cf7fd6727a1ee5bf0052
Mirella <mirella.lds@gmail.com>
2021-07-21 23:03:52 -0300

    Novo metodo de abertura de tela, aguardando um resultado.

--- app/src/main/java/com/example/helloandroid/TelaDeMensagemActivity.java
+++ app/src/main/java/com/example/helloandroid/TelaDeMensagemActivity.java
@@ -3,7 +3,9 @@ package com.example.helloandroid;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Instrumentation;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -46,4 +48,6 @@ public class TelaDeMensagemActivity extends AppCompatActivity {
             startActivityForResult(mensageiro, 123);
         }
     }
+
+
 }
