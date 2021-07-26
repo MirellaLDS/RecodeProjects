da45bfba7eed39e8be3d94781c5f6b3181823e39
Mirella <mirella.lds@gmail.com>
2021-07-25 22:50:55 -0300

    Aula 4 - ciclo de vida, Recuperar valores, Listagem

---
+++ app/src/main/java/com/example/helloandroid/ListagemActivity.java
@@ -0,0 +1,78 @@
+package com.example.helloandroid;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ListagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listagem);
+
+        ListView listView = findViewById(R.id.lvListagem);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, getListShortName());
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener((adapterView, view, position, id) -> {
+
+            String itemValue = (String) listView.getItemAtPosition(position);
+
+            String fullName = mapOfState().get(itemValue);
+
+            Toast.makeText(getApplicationContext(), fullName, Toast.LENGTH_SHORT).show();
+        });
+    }
+
+
+    private ArrayList<String> getListFullName() {
+        return new ArrayList<>(mapOfState().values());
+    }
+
+    private ArrayList<String> getListShortName() {
+        return new ArrayList<>(mapOfState().keySet());
+    }
+
+    private HashMap<String, String> mapOfState() {
+        HashMap<String, String> estados = new HashMap<>();
+        estados.put("AC", "Acre");
+        estados.put("AL", "Alagoas");
+        estados.put("AP", "Amapá");
+        estados.put("AM", "Amazonas");
+        estados.put("BA", "Bahia");
+        estados.put("CE", "Ceará");
+        estados.put("DF", "Distrito Federal");
+        estados.put("ES", "Espírito Santo");
+        estados.put("GO", "Goiás");
+        estados.put("MA", "Maranhão");
+        estados.put("MT", "Mato Grosso");
+        estados.put("MS", "Mato Grosso do Sul");
+        estados.put("MG", "Minas Gerais");
+        estados.put("PA", "Pará");
+        estados.put("PB", "Paraíba");
+        estados.put("PR", "Paraná");
+        estados.put("PE", "Pernambuco");
+        estados.put("PI", "Piauí");
+        estados.put("RJ", "Rio de Janeiro");
+        estados.put("RN", "Rio Grande do Norte");
+        estados.put("RS", "Rio Grande do Sul");
+        estados.put("RO", "Rondônia");
+        estados.put("RR", "Roraima");
+        estados.put("SC", "Santa Catarina");
+        estados.put("SP", "São Paulo");
+        estados.put("SE", "Sergipe");
+        estados.put("TO", "Tocantins");
+        return estados;
+    }
+
+}
--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -1,28 +1,52 @@
 package com.example.helloandroid;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String KEY = "key";
+
+    private TextView textView;
+    private EditText editText;
+    private Button btEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i(TAG, "onCreate");
+        Log.d(TAG, "onCreate");
 
-        // Inicializar referências de objetos e componentes
+        // Inicializar referências de objetos e componentes.
+        textView = findViewById(R.id.tvCampoDeTexto);
+        editText = findViewById(R.id.edTexto);
+        btEnviar = findViewById(R.id.btEnviar);
+
+        if (savedInstanceState != null) {
+            String revenue = savedInstanceState.getString(KEY, "Nada foi recuperado");
+            Log.d(TAG, "Valor recuperado: " + revenue);
+        }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG, "onStart");
+        Log.d(TAG, "onStart");
 
         // Estado intermediário, entre a criação da tela e a exibição para o usuário.
         // Buscar informações no banco, ou iniciar uma request.
@@ -32,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG, "onResume");
+        Log.d(TAG, "onResume");
 
         // Momento em que a tela está para ser exibida para o usuário.
         // Atualizar os componentes de tela com as informações necessárias.
@@ -42,23 +66,69 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-
+        Log.d(TAG, "onPause");
         // Tela não é o foco, mas o usuário ainda consegue visualiza-la.
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(TAG, "onStop");
+        Log.d(TAG, "onStop");
 
         // Tela não pode ser mais vizualizada.
+        // Exemplo: Quando a aplicação do youtube é tirada de foco, os vídeos param de reproduzir.
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState Called");
+
+        // Metodo para salvar o estado atual da tela teporáriamente, caso algum problema
+        // inesperado ocorra durante o uso do app.
+
+        String textoEscrito = editText.getText().toString();
+        outState.putString(KEY, textoEscrito);
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(TAG, "onDestroy");
+        Log.d(TAG, "onDestroy");
 
-        // Referência da aplicação foi destuída da memória.
+        // Informa que a aplicação será destruída da memória.
+    }
+
+    private void onShare() {
+        Intent shareIntent = ShareCompat.IntentBuilder.from(this)
+                .setText("Conteúdo que será compartilhado.")
+                .setType("text/plain")
+                .getIntent();
+        try {
+            startActivity(shareIntent);
+        } catch (ActivityNotFoundException ex) {
+            Toast.makeText(this, "Não é possível compatilhar.",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.shareMenuButton) {
+            onShare();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void exibirTexto(View view) {
+        String textoEscrito = editText.getText().toString();
+        textView.setText(textoEscrito);
     }
 }
